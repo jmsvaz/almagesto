02c5b80a298800bc8df61db6808ec4e63993d1fc
João Marcelo S. Vaz <jmsvaz@gmail.com>
2013-04-09 00:33:05 -0300

    added license header to almEarth

--- src/almEarth.pas
+++ src/almEarth.pas
@@ -1,3 +1,23 @@
+{
+    almEarth is part of Almagesto, a Free Pascal astronomical library.
+
+    Copyright (C) 2013 João Marcelo S. Vaz
+
+    Almagesto is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Almagesto is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+}
+
+//  This unit has Earth orientation routines.
 unit almEarth;
 
 {$mode objfpc}{$H+}
